70e1d43ad54e4441e2a6585524f5f012e7fa91a3
Abhimanyu <arya.abhimanyu.92@gmail.com>
2015-01-29 13:31:44 +0530

    first push

--- lib/geometry/line.rb
+++ lib/geometry/line.rb
@@ -1,11 +1,23 @@
 class Line
-	def initialize(xcord1,ycord1,xcord2,ycord2)
-		@xcord1 = xcord1
-		@ycord1 = ycord1
-		@xcord2 = xcord2
-		@ycord2 = ycord2
+	
+	attr_reader :point1, :point2
+	
+	def initialize(point1,point2)
+		@point1 = point1
+		@point2 = point2
 	end
+
+	def xcord1
+		@xcord1
+	end
+	
 	def calculateLength
-		((@xcord2-@xcord1)**2 + (@ycord2-@ycord1)**2)**0.5
+		((@point2[0]-@point1[0])**2 + (@point2[1]-@point1[1])**2)**0.5
 	end
+
+	
+    def ==(otherLine)
+    	(otherLine.class == Line) && (point1 == otherLine.point1 && point2==otherLine.point2) || (point2 == otherLine.point1 && point1==otherLine.point2)   		
+    end
+
 end
